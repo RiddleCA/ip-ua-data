2f26e53513a0305a7416d8f702521b099d130055
RiddleCA <66077069+RiddleCA@users.noreply.github.com>
2024-03-21 14:24:53 +1300

    Create git-actions.sh

---
+++ git-actions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+retries=0
+max_retries=3
+wait_time=10
+
+while [ $retries -lt $max_retries ]; do
+        git config --global user.email "support@sitehost.co.nz"
+        git config --global user.name "SiteHost NZ"
+        git commit -m "Filter expired rate-limits" || echo "Nothing to commit"
+        git pull --rebase origin main
+        git push origin HEAD:main
+
+    if [ $? -eq 0 ]; then
+        echo "Git commands executed successfully."
+        return 0
+    else
+        retries=$((retries + 1))
+        echo "Attempt $retries failed. Retrying..."
+        sleep $wait_time
+    fi
+done
+
+echo "All attempts failed. Exiting."
+exit 1
